7482c1c16670ef800de4f04e65213447dcec6740
ODENKITUNE <g021c1047@g.neec.ac.jp>
2022-12-14 05:42:13 +0900

    bug fix

--- G021C1047.sh
+++ G021C1047.sh
@@ -1,24 +1,21 @@
 #!/bin/bash
 function error() {
-    error_str = "error(" + "$1" + ")"
-    if expr $1 = 0 >&/dev/null;then
-        echo $error_str + " Two arguments are required. Usage: G021C1047.sh NUMBER NUMBER" >&2
+    error_str="error("$1")"
+    if [ $1 -eq 0 ];then
+        echo $error_str " Two arguments are required. Usage: G021C1047.sh NUMBER NUMBER" >&2
     else
-        echo $error_str + " Invalid argument. Usage: G021C1047.sh NUMBER NUMBER" >&2
+        echo $error_str " Invalid argument. Usage: G021C1047.sh NUMBER NUMBER" >&2
     fi
+    exit 1
 }
-if expr $# = "2" >&/dev/null;then
-    expr $1 + $2 >&/dev/null
-    if expr $? = 1 >&/dev/null;then
-        error 1
-        exit 1
-    elif expr $1 > $2 >&/dev/null;then
-        echo "bigger:" + "$1"
+if [ $# -eq 2 ];then
+    expr $1 + $2 &>/dev/null || error 1
+    if [ $1 -gt $2 ];then
+        echo "bigger:$1"
     else
-        echo "bigger:" + "$2"
+        echo "bigger:$2"
     fi
-    echo "sum:" + `expr $1 + $2`
+    echo "sum:" `expr $1 + $2`
 else
     error 0
-    exit 1
 fi
